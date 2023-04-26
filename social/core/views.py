09b952bd38ee7e9801e7a710275c9a7aa09a9f27
Julia Stamborovski <juliashizuko66@gmail.com>
2023-04-25 21:01:32 -0300

    search user

--- social/core/views.py
+++ social/core/views.py
@@ -48,6 +48,30 @@ def upload(request):
     else:
         return redirect('/')
 
+# SEARCH USERNAME
+@login_required(login_url= 'signin')
+def search(request):
+    user_object  = User.objects.get(username = request.user.username)
+    user_profile = Profile.objects.get(user = user_object)
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        username_object = User.objects.filter(username__icontains = username)
+        
+        username_profile = []
+        username_profile_list =[]
+        
+        for users in username_object:
+            username_profile.append(users.id)
+        
+        for ids in username_profile:
+            profile_lists = Profile.objects.filter(id_user=ids)
+            username_profile_list.append(profile_lists)   
+            
+    username_profile_list = list(chain(*username_profile_list))
+    
+    return render(request, 'search.html', {'user_profile': user_profile, 'username_profile_list': username_profile_list})
+
 # LIKES OF POST
 @login_required(login_url= 'signin')
 def like_post(request):
@@ -152,7 +176,6 @@ def settings(request):
     return render(request, "setting.html", {'user_profile': user_profile})
 
 # REGISTER
-
 def signup(request):
     if request.method == "POST":
         username = request.POST["username"]
